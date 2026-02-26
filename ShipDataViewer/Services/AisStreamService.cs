16577cf9a5ca25d6ab6bdadfdc2ce8d258189c5f
Igor Popov <igor@igorpopov.io>
2026-02-26 17:51:48 +0200

    Add code to parse messages

--- ShipDataViewer/Services/AisStreamService.cs
+++ ShipDataViewer/Services/AisStreamService.cs
@@ -1,8 +1,12 @@
-﻿using ShipDataViewer.Dtos;
+﻿using Org.OpenAPITools.Client;
+using Org.OpenAPITools.Model;
+
+using ShipDataViewer.Dtos;
 
 using System.Net.WebSockets;
 using System.Text;
 using System.Text.Json;
+using System.Text.Json.Serialization;
 
 namespace ShipDataViewer.Services;
 
@@ -11,9 +15,63 @@ public class AisStreamService : IService
 	private const string UriString = "wss://stream.aisstream.io/v0/stream";
 	private readonly ConnectionDetailsDto _connectionDetails;
 
+	private readonly JsonSerializerOptions _jsonOptions = new JsonSerializerOptions();
+
 	public AisStreamService(ConnectionDetailsDto connectionDetails)
 	{
 		_connectionDetails = connectionDetails;
+
+		_jsonOptions.Converters.Add(new JsonStringEnumConverter());
+		_jsonOptions.Converters.Add(new DateTimeJsonConverter());
+		_jsonOptions.Converters.Add(new DateTimeNullableJsonConverter());
+		_jsonOptions.Converters.Add(new DateOnlyJsonConverter());
+		_jsonOptions.Converters.Add(new DateOnlyNullableJsonConverter());
+		_jsonOptions.Converters.Add(new AddressedBinaryMessageJsonConverter());
+		_jsonOptions.Converters.Add(new AddressedBinaryMessageApplicationIDJsonConverter());
+		_jsonOptions.Converters.Add(new AddressedSafetyMessageJsonConverter());
+		_jsonOptions.Converters.Add(new AidsToNavigationReportJsonConverter());
+		_jsonOptions.Converters.Add(new AisMessageTypesJsonConverter());
+		_jsonOptions.Converters.Add(new AisMessageTypesNullableJsonConverter());
+		_jsonOptions.Converters.Add(new AisStreamMessageJsonConverter());
+		_jsonOptions.Converters.Add(new AisStreamMessageMessageJsonConverter());
+		_jsonOptions.Converters.Add(new AssignedModeCommandJsonConverter());
+		_jsonOptions.Converters.Add(new AssignedModeCommandCommandsJsonConverter());
+		_jsonOptions.Converters.Add(new AssignedModeCommandCommands0JsonConverter());
+		_jsonOptions.Converters.Add(new BaseStationReportJsonConverter());
+		_jsonOptions.Converters.Add(new BinaryAcknowledgeJsonConverter());
+		_jsonOptions.Converters.Add(new BinaryAcknowledgeDestinationsJsonConverter());
+		_jsonOptions.Converters.Add(new BinaryAcknowledgeDestinations0JsonConverter());
+		_jsonOptions.Converters.Add(new BinaryBroadcastMessageJsonConverter());
+		_jsonOptions.Converters.Add(new ChannelManagementJsonConverter());
+		_jsonOptions.Converters.Add(new ChannelManagementAreaJsonConverter());
+		_jsonOptions.Converters.Add(new ChannelManagementUnicastJsonConverter());
+		_jsonOptions.Converters.Add(new CoordinatedUTCInquiryJsonConverter());
+		_jsonOptions.Converters.Add(new DataLinkManagementMessageJsonConverter());
+		_jsonOptions.Converters.Add(new DataLinkManagementMessageDataJsonConverter());
+		_jsonOptions.Converters.Add(new DataLinkManagementMessageData0JsonConverter());
+		_jsonOptions.Converters.Add(new ErrorJsonConverter());
+		_jsonOptions.Converters.Add(new ExtendedClassBPositionReportJsonConverter());
+		_jsonOptions.Converters.Add(new GnssBroadcastBinaryMessageJsonConverter());
+		_jsonOptions.Converters.Add(new GroupAssignmentCommandJsonConverter());
+		_jsonOptions.Converters.Add(new InterrogationJsonConverter());
+		_jsonOptions.Converters.Add(new InterrogationStation1Msg1JsonConverter());
+		_jsonOptions.Converters.Add(new InterrogationStation1Msg2JsonConverter());
+		_jsonOptions.Converters.Add(new InterrogationStation2JsonConverter());
+		_jsonOptions.Converters.Add(new LongRangeAisBroadcastMessageJsonConverter());
+		_jsonOptions.Converters.Add(new MultiSlotBinaryMessageJsonConverter());
+		_jsonOptions.Converters.Add(new PositionReportJsonConverter());
+		_jsonOptions.Converters.Add(new SafetyBroadcastMessageJsonConverter());
+		_jsonOptions.Converters.Add(new ShipStaticDataJsonConverter());
+		_jsonOptions.Converters.Add(new ShipStaticDataDimensionJsonConverter());
+		_jsonOptions.Converters.Add(new ShipStaticDataEtaJsonConverter());
+		_jsonOptions.Converters.Add(new SingleSlotBinaryMessageJsonConverter());
+		_jsonOptions.Converters.Add(new StandardClassBPositionReportJsonConverter());
+		_jsonOptions.Converters.Add(new StandardSearchAndRescueAircraftReportJsonConverter());
+		_jsonOptions.Converters.Add(new StaticDataReportJsonConverter());
+		_jsonOptions.Converters.Add(new StaticDataReportReportAJsonConverter());
+		_jsonOptions.Converters.Add(new StaticDataReportReportBJsonConverter());
+		_jsonOptions.Converters.Add(new SubscriptionMessageJsonConverter());
+		_jsonOptions.Converters.Add(new UnknownMessageJsonConverter());
 	}
 
 	public async Task Listen(CancellationToken token = default)
@@ -35,7 +93,8 @@ public class AisStreamService : IService
 				return;
 			}
 
-			var x = $"Received {Encoding.Default.GetString(buffer, 0, result.Count)}";
+			var responseJson = Encoding.Default.GetString(buffer, 0, result.Count);
+			var obj = JsonSerializer.Deserialize<AisStreamMessage>(responseJson, _jsonOptions);
 		}
 	}
 }
