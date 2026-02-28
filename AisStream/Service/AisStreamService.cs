2fe3efbfc7c3f1d5cc5e32f5466fa03e8e4d2af5
Igor Popov <igor@igorpopov.io>
2026-02-28 12:34:07 +0200

    Use reflection to register json converters
    Signed-off-by: Igor Popov <igor@igorpopov.io>

--- AisStream/Service/AisStreamService.cs
+++ AisStream/Service/AisStreamService.cs
@@ -1,5 +1,4 @@
-﻿using Org.OpenAPITools.Client;
-using Org.OpenAPITools.Model;
+﻿using Org.OpenAPITools.Model;
 
 using ShipDataViewer.Core.Model;
 using ShipDataViewer.Core.Service;
@@ -17,76 +16,48 @@ public class AisStreamService : IService
 
 	private readonly ServiceConfiguration _serviceConfiguration;
 
-	private readonly JsonSerializerOptions _jsonOptions = new JsonSerializerOptions();
+	private readonly JsonSerializerOptions _jsonSerializerOptions;
+
+	public event EventHandler<Ship>? ShipDataReceived;
 
 	public AisStreamService(ServiceConfiguration serviceConfiguration)
 	{
 		_serviceConfiguration = serviceConfiguration;
-
-		_jsonOptions.Converters.Add(new JsonStringEnumConverter());
-		_jsonOptions.Converters.Add(new DateTimeJsonConverter());
-		_jsonOptions.Converters.Add(new DateTimeNullableJsonConverter());
-		_jsonOptions.Converters.Add(new DateOnlyJsonConverter());
-		_jsonOptions.Converters.Add(new DateOnlyNullableJsonConverter());
-		_jsonOptions.Converters.Add(new AddressedBinaryMessageJsonConverter());
-		_jsonOptions.Converters.Add(new AddressedBinaryMessageApplicationIDJsonConverter());
-		_jsonOptions.Converters.Add(new AddressedSafetyMessageJsonConverter());
-		_jsonOptions.Converters.Add(new AidsToNavigationReportJsonConverter());
-		_jsonOptions.Converters.Add(new AisMessageTypesJsonConverter());
-		_jsonOptions.Converters.Add(new AisMessageTypesNullableJsonConverter());
-		_jsonOptions.Converters.Add(new AisStreamMessageJsonConverter());
-		_jsonOptions.Converters.Add(new AisStreamMessageMessageJsonConverter());
-		_jsonOptions.Converters.Add(new AssignedModeCommandJsonConverter());
-		_jsonOptions.Converters.Add(new AssignedModeCommandCommandsJsonConverter());
-		_jsonOptions.Converters.Add(new AssignedModeCommandCommands0JsonConverter());
-		_jsonOptions.Converters.Add(new BaseStationReportJsonConverter());
-		_jsonOptions.Converters.Add(new BinaryAcknowledgeJsonConverter());
-		_jsonOptions.Converters.Add(new BinaryAcknowledgeDestinationsJsonConverter());
-		_jsonOptions.Converters.Add(new BinaryAcknowledgeDestinations0JsonConverter());
-		_jsonOptions.Converters.Add(new BinaryBroadcastMessageJsonConverter());
-		_jsonOptions.Converters.Add(new ChannelManagementJsonConverter());
-		_jsonOptions.Converters.Add(new ChannelManagementAreaJsonConverter());
-		_jsonOptions.Converters.Add(new ChannelManagementUnicastJsonConverter());
-		_jsonOptions.Converters.Add(new CoordinatedUTCInquiryJsonConverter());
-		_jsonOptions.Converters.Add(new DataLinkManagementMessageJsonConverter());
-		_jsonOptions.Converters.Add(new DataLinkManagementMessageDataJsonConverter());
-		_jsonOptions.Converters.Add(new DataLinkManagementMessageData0JsonConverter());
-		_jsonOptions.Converters.Add(new ErrorJsonConverter());
-		_jsonOptions.Converters.Add(new ExtendedClassBPositionReportJsonConverter());
-		_jsonOptions.Converters.Add(new GnssBroadcastBinaryMessageJsonConverter());
-		_jsonOptions.Converters.Add(new GroupAssignmentCommandJsonConverter());
-		_jsonOptions.Converters.Add(new InterrogationJsonConverter());
-		_jsonOptions.Converters.Add(new InterrogationStation1Msg1JsonConverter());
-		_jsonOptions.Converters.Add(new InterrogationStation1Msg2JsonConverter());
-		_jsonOptions.Converters.Add(new InterrogationStation2JsonConverter());
-		_jsonOptions.Converters.Add(new LongRangeAisBroadcastMessageJsonConverter());
-		_jsonOptions.Converters.Add(new MultiSlotBinaryMessageJsonConverter());
-		_jsonOptions.Converters.Add(new PositionReportJsonConverter());
-		_jsonOptions.Converters.Add(new SafetyBroadcastMessageJsonConverter());
-		_jsonOptions.Converters.Add(new ShipStaticDataJsonConverter());
-		_jsonOptions.Converters.Add(new ShipStaticDataDimensionJsonConverter());
-		_jsonOptions.Converters.Add(new ShipStaticDataEtaJsonConverter());
-		_jsonOptions.Converters.Add(new SingleSlotBinaryMessageJsonConverter());
-		_jsonOptions.Converters.Add(new StandardClassBPositionReportJsonConverter());
-		_jsonOptions.Converters.Add(new StandardSearchAndRescueAircraftReportJsonConverter());
-		_jsonOptions.Converters.Add(new StaticDataReportJsonConverter());
-		_jsonOptions.Converters.Add(new StaticDataReportReportAJsonConverter());
-		_jsonOptions.Converters.Add(new StaticDataReportReportBJsonConverter());
-		_jsonOptions.Converters.Add(new SubscriptionMessageJsonConverter());
-		_jsonOptions.Converters.Add(new UnknownMessageJsonConverter());
+		_jsonSerializerOptions = CreateJsonOptions();
 	}
 
-	public event EventHandler<Ship>? ShipDataReceived;
+	private JsonSerializerOptions CreateJsonOptions()
+	{
+		var assembly = typeof(AisStreamMessage).Assembly;
+		var converterTypes = assembly
+			.GetTypes()
+			.Where(t =>
+				!t.IsAbstract &&
+				!t.IsInterface &&
+				typeof(JsonConverter).IsAssignableFrom(t))
+			.ToList();
+
+		var options = new JsonSerializerOptions();
+		foreach (var type in converterTypes)
+		{
+			if (Activator.CreateInstance(type) is JsonConverter converter)
+			{
+				options.Converters.Add(converter);
+			}
+		}
+
+		return options;
+	}
 
 	public async Task ListenAsync(CancellationToken token = default)
 	{
-		var connectionDetailsJson = JsonSerializer.Serialize(_serviceConfiguration);
-
 		using var webSocket = new ClientWebSocket();
 		await webSocket.ConnectAsync(new Uri(UriString), token);
-		await webSocket.SendAsync(new ArraySegment<byte>(Encoding.UTF8.GetBytes(connectionDetailsJson)), WebSocketMessageType.Text, true, token);
-		var buffer = new byte[4096];
 
+		var serviceConfigurationJson = JsonSerializer.Serialize(_serviceConfiguration);
+		await webSocket.SendAsync(new ArraySegment<byte>(Encoding.UTF8.GetBytes(serviceConfigurationJson)), WebSocketMessageType.Text, true, token);
+
+		var buffer = new byte[4096];
 		while (webSocket.State == WebSocketState.Open)
 		{
 			var response = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), token);
@@ -98,7 +69,7 @@ public class AisStreamService : IService
 			}
 
 			var responseJson = Encoding.UTF8.GetString(buffer, 0, response.Count);
-			var aisStreamMessage = JsonSerializer.Deserialize<AisStreamMessage>(responseJson, _jsonOptions);
+			var aisStreamMessage = JsonSerializer.Deserialize<AisStreamMessage>(responseJson, _jsonSerializerOptions);
 
 			if (aisStreamMessage == null)
 			{
