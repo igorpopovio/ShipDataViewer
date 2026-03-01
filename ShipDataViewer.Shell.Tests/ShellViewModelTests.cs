a3dc91750547ca0b53e1c0a418d4b6448358ad37
Igor Popov <igor@igorpopov.io>
2026-03-01 10:16:39 +0200

    Add test project
    Signed-off-by: Igor Popov <igor@igorpopov.io>

---
+++ ShipDataViewer.Shell.Tests/ShellViewModelTests.cs
@@ -0,0 +1,15 @@
+﻿using Autofac.Extras.Moq;
+
+namespace ShipDataViewer.Shell.Tests;
+
+public class ShellViewModelTests
+{
+	[Test]
+	public void HasDisplayName()
+	{
+		using var mock = AutoMock.GetLoose();
+		var shellViewModel = mock.Create<ShellViewModel>();
+
+		Assert.That(shellViewModel.DisplayName, Is.EqualTo("Ship Data Viewer"));
+	}
+}
