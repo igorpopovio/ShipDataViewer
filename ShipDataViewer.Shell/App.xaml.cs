0f581a04d1d9ba700e34ad896f44d12af23e8a65
Igor Popov <igor@igorpopov.io>
2026-03-02 15:25:38 +0200

    Configure logging
    Signed-off-by: Igor Popov <igor@igorpopov.io>

--- ShipDataViewer.Shell/App.xaml.cs
+++ ShipDataViewer.Shell/App.xaml.cs
@@ -1,5 +1,18 @@
-﻿namespace ShipDataViewer;
+﻿using Serilog;
 
-public partial class App
+using System.Windows;
+
+namespace ShipDataViewer;
+
+public partial class App : Application
 {
+	protected override void OnStartup(StartupEventArgs e)
+	{
+		base.OnStartup(e);
+
+		Log.Logger = new LoggerConfiguration()
+			.WriteTo.File("log.txt", rollingInterval: RollingInterval.Day)
+			.CreateLogger();
+		Log.Information("Application has started!");
+	}
 }
