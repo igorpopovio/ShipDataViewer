b8ef1674300ea4d63ebdfdeec234052bb0eb54b8
Igor Popov <igor@igorpopov.io>
2026-03-11 16:10:34 +0200

    Add RandomlyGeneratedAiStreamService
    Signed-off-by: Igor Popov <igor@igorpopov.io>

--- App/ShipDataViewer.Shell/Bootstrapper.cs
+++ App/ShipDataViewer.Shell/Bootstrapper.cs
@@ -1,6 +1,6 @@
-﻿using AisStream.Service;
+﻿using Autofac;
 
-using Autofac;
+using RandomlyGeneratedAisStream.Service;
 
 namespace ShipDataViewer.Shell;
 
@@ -9,7 +9,8 @@ public class Bootstrapper : AutofacBootstrapper<ShellViewModel>
 	protected override void ConfigureIoC(ContainerBuilder builder)
 	{
 		base.ConfigureIoC(builder);
-		builder.RegisterType<AisStreamService>().AsImplementedInterfaces();
+		//builder.RegisterType<AisStreamService>().AsImplementedInterfaces();
+		builder.RegisterType<RandomlyGeneratedAiStreamService>().AsImplementedInterfaces();
 		//builder.RegisterType<AisWebSocketStreamService>().AsImplementedInterfaces();
 	}
 }
---
+++ Services/RandomlyGeneratedAisStream/Service/RandomlyGeneratedAiStreamService.cs
@@ -0,0 +1,47 @@
+﻿using ShipDataViewer.Core.Model;
+using ShipDataViewer.Core.Service;
+
+namespace RandomlyGeneratedAisStream.Service;
+
+public class RandomlyGeneratedAiStreamService : IService
+{
+	public event EventHandler<Ship>? ShipDataReceived;
+	public event EventHandler<Position>? PositionDataReceived;
+
+	public async Task ListenAsync(CancellationToken token = default)
+	{
+		var ships = new List<Ship>();
+		for (int i = 0; i < 50 && !token.IsCancellationRequested; i++)
+		{
+			var ship = new Ship
+			{
+				Mmsi = i,
+				ImoNumber = i,
+				CallSign = $"Call Sign {i}",
+				Name = $"Ship{i}",
+			};
+			ships.Add(ship);
+			ShipDataReceived?.Invoke(this, ship);
+		}
+
+		var random = new Random();
+		while (!token.IsCancellationRequested)
+		{
+			foreach (var ship in ships)
+			{
+				var position = new Position
+				{
+					ShipMmsi = ship.Mmsi,
+					Latitude = -90 + random.NextDouble() * 180,
+					Longitude = -180 + random.NextDouble() * 360,
+					Sog = random.NextDouble(),
+					Cog = random.NextDouble(),
+					TrueHeading = random.Next(),
+				};
+				PositionDataReceived?.Invoke(this, position);
+			}
+
+			await Task.Delay(TimeSpan.FromMilliseconds(500), token);
+		}
+	}
+}
